6d2a271caa6689c89755aa2c8ef8e1d83365ec85
Nonthefirst <wmofficial0@163.com>
2025-04-28 13:37:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vRYlYvQor-S-T9MioCkk6iH3frHP949J6AvkYp4ffG40hLHmI8QaOiAMIx83aAc9bJCGfBdxc6A4BJO/pubembed?start=false&loop=false&delayms=3000", height=500)
+components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vSMdHc2BVsq_cKaL1kxWA4OUK7xUa-NkpQWHxi-O5rD6xiI20z6QRqfkIR_kYymSUWNI4lpwup7wjEY/pubembed?start=false&loop=false&delayms=3000", height=500)
