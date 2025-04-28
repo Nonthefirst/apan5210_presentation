ddaba3f88ae2d250d0b43f25b75fdb40ac8d5b98
Nonthefirst <wmofficial0@163.com>
2025-04-28 12:29:58 -0400

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vRYlYvQor-S-T9MioCkk6iH3frHP949J6AvkYp4ffG40hLHmI8QaOiAMIx83aAc9bJCGfBdxc6A4BJO/pubembed?start=false&loop=false&delayms=3000", height=500)
